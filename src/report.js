91dba76a66cdb85f1e4abd7a69a80acf2dd4b1ef
mirahmed39 <mir.ahmed39@yahoo.com>
2017-09-28 00:38:18 -0400

    implement part3 function using recursion

--- src/report.js
+++ src/report.js
@@ -1,21 +1,40 @@
-const fs = require('fs');
+const request = require('request');
 const app = require('./yelpfunc');
 
-fs.readFile('C:\\Users\\mirah\\Dropbox\\NYU-classes\\5th_semester\\Applied_Internet_Tech\\homeworks\\homework02-data\\business.json', 'utf8', (err, data) =>{
-    if (err) {
-        console.log(err);
-        console.log("Error reading a file");
-    }
-    let jsonObjects = data.trim();
-    jsonObjects = jsonObjects.split('\n');
-    jsonObjects = jsonObjects.slice(0, jsonObjects.length -1);
-    const parsedObjects = jsonObjects.map(function (ele) {
-        let trimmedString = ele.trim();
-        return JSON.parse(trimmedString);
-    });
+let baseUrl = "https://foureyes.github.io/csci-ua.0480-fall2017-003/homework/02/";
+let currentUrl = "086e27c89913c5c2dde62b6cdd5a27d2.json";
 
-    console.log(app.processYelpData(parsedObjects));
-});
+parseJsonFiles(baseUrl+currentUrl);
+
+function parseJsonFiles(url) {
+    request(url, function (error, response, body) {
+        if (error)
+            console.log('Error reading file');
+        else {
+            let jsonObjects = body.trim();
+            jsonObjects = jsonObjects.split('\n');
+            let parsedObjects = jsonObjects.map(function (ele) {
+                return JSON.parse(ele);
+            });
+            if (parsedObjects[parsedObjects.length-1].hasOwnProperty("nextFile")) {
+                let jsonObjectsSliced = parsedObjects.slice(0, jsonObjects.length - 1);
+                console.log("==============================");
+                console.log(baseUrl + currentUrl);
+                console.log("==============================");
+                console.log(app.processYelpData(jsonObjectsSliced));
+                currentUrl = parsedObjects[parsedObjects.length -1].nextFile;
+                parseJsonFiles(baseUrl+currentUrl);
+            } else {
+                console.log("==============================");
+                console.log(baseUrl + currentUrl);
+                console.log("==============================");
+                console.log(app.processYelpData(parsedObjects));
+            }
+        }
+    });
+}
+
+
 
 
 
